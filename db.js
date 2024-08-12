f4e050613f0e9f31bb2ca547dcf253ba25856711
HITESH KHANDELWAL <hiteshkhandelwal2002@gmail.com>
2024-08-12 16:07:27 +0530

    Final commit

--- db.js
+++ db.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const mongoURI = "mongodb://localhost:27017/?directConnection=true&readPreference=primary";
+const mongoURI =
+  "mongodb://localhost:27017/?directConnection=true&readPreference=primary";
 
 const connectToMongo = async () => {
-    try {
-        await mongoose.connect(mongoURI);
-        console.log("Connected to MongoDB");
-    } catch (err) {
-        console.error("Failed to connect to MongoDB", err);
-    }
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+  }
 };
 
 module.exports = connectToMongo;
